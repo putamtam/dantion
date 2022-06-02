edab583b851d07d7d088854f2c31b4471dc030b8
Audi Adyan <audiadyan123@gmail.com>
2022-06-02 17:48:24 +0800

    edit photourl

--- controllers/users.js
+++ controllers/users.js
@@ -250,7 +250,7 @@ export const userUpdate = async (req, res) => {
             parentNumber: parentNumber, 
             email: email,
             password: hashPassword(password), 
-            photo: photo, 
+            photo: photoUrl, 
             updatedAt: new Date().toISOString()
         }
     };
